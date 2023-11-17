765ce40c8fc05066e5f129d3995b4bab0215b4df
Alec Reynolds <alec+git@lando.dev>
2023-11-17 15:02:38 -0800

    Feed linter.

--- builders/php-nginx.js
+++ builders/php-nginx.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const _ = require('lodash');
-const LandoNginx = require('./../../node_modules/@lando/nginx/builders/nginx.js');
+const landoNginx = require('./../node_modules/@lando/nginx/builders/nginx.js');
 
 // Builder
 module.exports = {
   name: 'php-nginx',
   parent: '_service',
-  builder: (parent, config) => class PhpNginx extends LandoNginx(parent, config) {
+  builder: (parent, config) => class PhpNginx extends landoNginx(parent, landoNginx.config) {
     constructor(id, options = {}) {
       super(id, options, {services: _.set({}, options.name)});
     };
