35f8aba881a41e91eb92e9c949fd41f0619f3eb3
Alec Reynolds <alec+git@lando.dev>
2024-10-08 13:46:37 -0700

    Update docs build to create multi-version builds.

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -19,6 +19,9 @@ export default defineConfig({
     ['link', {rel: 'icon', href: '/php/favicon.svg', type: 'image/svg+xml'}],
   ],
   themeConfig: {
+    multiVersionBuild: {
+      satisfies: '>=1.2.0',
+    },
     sidebar: sidebar(),
   },
 });
